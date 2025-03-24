b63adf2b9f0a22cc3cc97738421a0458c6fe0aeb
ayaahmed2058 <ayaahmedmohamed85@gmail.com>
2025-03-24 10:30:54 +0200

    Search bar Task

--- app/src/main/java/com/example/searchbar/MainActivity.kt
+++ app/src/main/java/com/example/searchbar/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.searchbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
