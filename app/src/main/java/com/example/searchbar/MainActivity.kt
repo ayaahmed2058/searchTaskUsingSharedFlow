1dfcbe4109e8fbc4d8ff5c6fe3d2bafa5f342c5c
ayaahmed2058 <ayaahmedmohamed85@gmail.com>
2025-03-24 10:25:16 +0200

    Search bar Task

--- app/src/main/java/com/example/searchbar/MainActivity.kt
+++ app/src/main/java/com/example/searchbar/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import androidx.compose.runtime.LaunchedEffect as LaunchedEffect1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,41 +25,52 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(FlowPreview::class)
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun SearchScreen() {
     val names = listOf("Aya", "Ahmed", "Mohamed", "ITI", "android", "Smart", "village")
     val searchView = remember { MutableSharedFlow<String>() }
-    var userInput = remember { mutableStateOf("") }
+    var userInput = remember { mutableStateOf(value = "")}
     var searchNames =  remember { mutableStateOf(names) }
 
-    LaunchedEffect1(Unit) {
+    LaunchedEffect(Unit) {
         searchView
             .debounce(100)
             .collect { query ->
-                searchNames.value = names.filter { it.startsWith(query, ignoreCase = true) }
+                searchNames.value = names.filter { it.contains(query, ignoreCase = true) }
             }
     }
 
     val coroutineScope = rememberCoroutineScope()
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column (
+        modifier = Modifier.fillMaxSize()
+            .padding(18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
         TextField(
             value = userInput.value,
-            onValueChange = { name ->
-                userInput.value = name
+            onValueChange = {
+                userInput.value = it
                 coroutineScope.launch {
-                    searchView.emit(name)
+                    searchView.emit(it)
                 }
             },
-            shape = RoundedCornerShape(18.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .size(60.dp)
-                .padding(16.dp)
+            label = { Text(text = "Search") },
         )
+
         Spacer(modifier = Modifier.height(16.dp))
-        searchNames.value.forEach { name ->
-            Text(text = name)
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(searchNames.value.size) { index ->
+                Text(
+                    text = searchNames.value[index],
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+            }
         }
 
     }
